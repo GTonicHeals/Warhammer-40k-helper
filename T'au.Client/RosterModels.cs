ababcaa48a021676c3d971fd94a4caa41c5aa8ba
GTonic <nicodaf@gmail.com>
2026-02-08 00:27:54 -0500

    Add project files.

---
+++ T'au.Client/RosterModels.cs
@@ -0,0 +1,78 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WarHammerRoster.Models
+{
+    public class RosterRoot
+    {
+        [JsonPropertyName("roster")]
+        public Roster Roster { get; set; }
+    }
+
+    public class Roster
+    {
+        [JsonPropertyName("forces")]
+        public List<Force> Forces { get; set; } = new();
+
+        [JsonPropertyName("costs")]
+        public List<Cost> Costs { get; set; } = new();
+
+        [JsonPropertyName("costLimits")]
+        public List<Cost> CostLimits { get; set; } = new();
+    }
+
+    public class Force
+    {
+        [JsonPropertyName("selections")]
+        public List<Selection> Selections { get; set; } = new();
+    }
+
+    public class Selection
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("number")]
+        public int Number { get; set; }
+
+        // Recursive: Units have weapons, which are also "Selections"
+        [JsonPropertyName("selections")]
+        public List<Selection> Selections { get; set; } = new();
+
+        [JsonPropertyName("profiles")]
+        public List<Profile> Profiles { get; set; } = new();
+
+        [JsonPropertyName("costs")]
+        public List<Cost> Costs { get; set; } = new();
+    }
+
+    public class Profile
+    {
+        [JsonPropertyName("typeName")]
+        public string TypeName { get; set; } // e.g., "Unit", "Abilities", "Ranged Weapons"
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("characteristics")]
+        public List<Characteristic> Characteristics { get; set; } = new();
+    }
+
+    public class Characteristic
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        // The JSON uses "$text" for the value
+        [JsonPropertyName("$text")]
+        public string Value { get; set; }
+    }
+
+    public class Cost
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("value")]
+        public double Value { get; set; }
+    }
+}
---
+++ T'au/RosterModels.cs
@@ -0,0 +1,104 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WarHammerRoster.Models
+{
+    public class RosterRoot
+    {
+        [JsonPropertyName("roster")]
+        public Roster Roster { get; set; }
+    }
+
+    public class Roster
+    {
+        [JsonPropertyName("forces")]
+        public List<Force> Forces { get; set; } = new();
+
+        [JsonPropertyName("costs")]
+        public List<Cost> Costs { get; set; } = new();
+
+        [JsonPropertyName("costLimits")]
+        public List<Cost> CostLimits { get; set; } = new();
+    }
+
+    public class Force
+    {
+        [JsonPropertyName("selections")]
+        public List<Selection> Selections { get; set; } = new();
+
+        [JsonPropertyName("rules")]
+        public List<Rule> Rules { get; set; } = new();
+    }
+
+    public class Selection
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("number")]
+        public int Number { get; set; }
+
+        [JsonPropertyName("selections")]
+        public List<Selection> Selections { get; set; } = new();
+
+        [JsonPropertyName("profiles")]
+        public List<Profile> Profiles { get; set; } = new();
+
+        [JsonPropertyName("rules")]
+        public List<Rule> Rules { get; set; } = new(); // <--- ADDED
+
+        [JsonPropertyName("categories")]
+        public List<Category> Categories { get; set; } = new(); // <--- ADDED
+
+        [JsonPropertyName("costs")]
+        public List<Cost> Costs { get; set; } = new();
+    }
+
+    public class Profile
+    {
+        [JsonPropertyName("typeName")]
+        public string TypeName { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("characteristics")]
+        public List<Characteristic> Characteristics { get; set; } = new();
+    }
+
+    public class Characteristic
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("$text")]
+        public string Value { get; set; }
+    }
+
+    public class Cost
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("value")]
+        public double Value { get; set; }
+    }
+
+    // --- NEW CLASSES ---
+    public class Rule
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("primary")]
+        public bool Primary { get; set; }
+    }
+}
